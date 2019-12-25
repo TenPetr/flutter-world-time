65bdcd2b0b6f1f95cc56cabfb68a1df1d8960215
Petr Jelínek <peetr.jelinek@gmail.com>
2019-12-25 11:53:03 +0100

    Added routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:worldtime/pages/home.dart';
+import 'package:worldtime/pages/loading.dart';
+import 'package:worldtime/pages/location.dart';
 
-void main() => runApp(MaterialApp(home: Home()));
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
+void main() => runApp(
+      MaterialApp(
+        initialRoute: "/home",
+        routes: {
+          "/": (context) => Loading(),
+          "/home": (context) => Home(),
+          "/location": (context) => Location(),
+        },
+      ),
+    );
